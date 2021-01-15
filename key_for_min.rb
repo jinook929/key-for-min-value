25fa2fa99ca9d2a13147c81015d0a9d322d7ac86
jinook929 <jinook929@gmail.com>
2021-01-15 09:11:18 -0500

    commit tree c305a3c4db64c3b049f071312ea7c834ded441d3

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,43 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+    # arr = name_hash.collect { |k, v|
+    #     [k, v]
+    # }
+    
+    # min_name = nil
+    # min = nil
+    # counter = 0
+    # arr.each {|e|
+    #     counter += 1
+    #     if counter == 1
+    #         min_name = e[0]
+    #         min = e[1]
+    #     else
+    #         if e[1] < min
+    #             min_name = e[0]
+    #             min = e[1]
+    #         end
+    #     end
+    # }
+    min_name = nil
+    min = nil
+    counter = 0
+    name_hash.each {|k, v|
+        counter += 1
+        if counter == 1
+            min_name = k
+            min = v
+        else
+            if v < min
+                min_name = k
+                min = v
+            end
+        end
+    }
 
+    min_name
 end
+
+# hash = { :blake => 500, :son => 1, :ashley => 2 }
+# p key_for_min_value(hash)
